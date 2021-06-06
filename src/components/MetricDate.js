ef314c783d48e3c13d9a15eb5fb3256e1106cd12
HabibehKhavari <h.khavari1995@gmail.com>
2021-06-06 14:58:40 +0300

    split components: create MetricDate

---
+++ src/components/MetricDate.js
@@ -0,0 +1,13 @@
+import './MetricDate.css';
+
+const MetricDate = (props) => {
+  const dispDate = props.userDate.toLocaleString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  });
+
+  return <div className="metric-item__date">{dispDate}</div>;
+};
+
+export default MetricDate;
--- src/components/MetricItem.js
+++ src/components/MetricItem.js
@@ -1,18 +1,17 @@
-import "./MetricItem.css"
+import './MetricItem.css';
+import MetricDate from './MetricDate';
 
-const MetricItem = (props)=>{
-
-    const dispDate = props.userDate.toLocaleString("en-US", 
-        {hour: "2-digit", minute: "2-digit", hour12: true});
-
-    return <div className="metric-item">
-    <div className="metric-item__date">{dispDate}</div>
-    <div className="metric-item__metric">
+const MetricItem = (props) => {
+  return (
+    <div className="metric-item">
+      <MetricDate userDate={props.userDate} />
+      <div className="metric-item__metric">
         <div>{props.userAmount}</div>
         <h3>{props.userMeal}</h3>
         <h1>{props.userName}</h1>
+      </div>
+      <div className="metric-item__count">{props.userCalorei}</div>
     </div>
-    <div className="metric-item__count">{props.userCalorei}</div>
-</div>
-}
-export default MetricItem
+  );
+};
+export default MetricItem;
