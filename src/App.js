9beaff3a77d02d6481199f777b546d91994a0fd9
HabibehKhavari <h.khavari1995@gmail.com>
2021-06-06 12:52:30 +0300

    Add MetricItem: value as props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import MetricItem from './components/MetricItem'
 
 function App() {
+  let userDate=new Date(2021,5,2,13,22,0,0);
+  let userName="Habibeh";
+  let userMeal="Lunch";
+  let userCalorei="45 cal";
+  let userAmount="100gr burger";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Hello</h1>
+    <MetricItem
+    userDate={userDate}
+    userName={userName}
+    userMeal={userMeal}
+    userCalorei={userCalorei}
+    userAmount={userAmount}
+    />
     </div>
   );
 }
