299be4b0de62103036a958062c10a2c7d46c20fc
HabibehKhavari <h.khavari1995@gmail.com>
2021-06-06 14:20:26 +0300

    use js in order to format date

--- src/components/MetricItem.js
+++ src/components/MetricItem.js
@@ -1,8 +1,12 @@
 import "./MetricItem.css"
 
 const MetricItem = (props)=>{
+
+    const dispDate = props.userDate.toLocaleString("en-US", 
+        {hour: "2-digit", minute: "2-digit", hour12: true});
+
     return <div className="metric-item">
-    <div className="metric-item__date">{props.userDate.toISOString()}</div>
+    <div className="metric-item__date">{dispDate}</div>
     <div className="metric-item__metric">
         <div>{props.userAmount}</div>
         <h3>{props.userMeal}</h3>
