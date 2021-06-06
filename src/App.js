74e3a48f22f97285ad55f19e69979ca82b489bf0
HabibehKhavari <h.khavari1995@gmail.com>
2021-06-06 14:10:57 +0300

    use array of objects

--- src/App.js
+++ src/App.js
@@ -3,21 +3,39 @@ import './App.css';
 import MetricItem from './components/MetricItem'
 
 function App() {
-  let userDate=new Date(2021,5,2,13,22,0,0);
-  let userName="Habibeh";
-  let userMeal="Lunch";
-  let userCalorei="45 cal";
-  let userAmount="100gr burger";
+  const metrics = [
+    {
+      date:new Date(2021,5,2,13,22,0,0),
+      name:"Habibeh",
+      meal:"Lunch",
+      calorei:"45 cal",
+      amount:"100gr burger"
+    },
+    {
+      date:new Date(2021,5,5,10,22,0,0),
+      name:"Melika",
+      meal:"Breakfast",
+      calorei:"45 cal",
+      amount:"100gr butter"
+    }
+  ]
 
   return (
     <div>
     <h1>Hello</h1>
     <MetricItem
-    userDate={userDate}
-    userName={userName}
-    userMeal={userMeal}
-    userCalorei={userCalorei}
-    userAmount={userAmount}
+    userDate={metrics[0].date}
+    userName={metrics[0].name}
+    userMeal={metrics[0].meal}
+    userCalorei={metrics[0].calorei}
+    userAmount={metrics[0].amount}
+    />
+    <MetricItem
+    userDate={metrics[1].date}
+    userName={metrics[1].name}
+    userMeal={metrics[1].meal}
+    userCalorei={metrics[1].calorei}
+    userAmount={metrics[1].amount}
     />
     </div>
   );
