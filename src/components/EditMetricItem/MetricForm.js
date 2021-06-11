0e9e2f197be1ffed4976bed514c38fa9d48eed85
HabibehKhavari <h.khavari1995@gmail.com>
2021-06-11 13:22:50 +0300

    add metric: read user input

--- src/components/EditMetricItem/MetricForm.js
+++ src/components/EditMetricItem/MetricForm.js
@@ -1,28 +1,69 @@
 import './MetricForm.css';
+import { useState } from 'react';
 
 const MetricForm = (props) => {
+  const [userData, setUserData] = useState('');
+  const dateChangeHandler = (event) => {
+    let newValue = event.target.value;
+    setUserData(newValue);
+    console.log(`someone typed ${userData}`);
+  };
+
+  const [userName, setUserName] = useState('');
+  const nameChangeHandler = (event) => {
+    let newValue = event.target.value;
+    setUserName(newValue);
+    console.log(`someone typed ${userName}`);
+  };
+
+  const [userMeal, setUserMeal] = useState('');
+  const mealChangeHandler = (event) => {
+    let newValue = event.target.value;
+    setUserMeal(newValue);
+    console.log(`someone typed ${userMeal}`);
+  };
+
+  const [userAmount, setUserAmount] = useState('');
+  const amountChangeHandler = (event) => {
+    let newValue = event.target.value;
+    setUserAmount(newValue);
+    console.log(`someone typed ${userAmount}`);
+  };
+
+  const [userCalorei, setUserCalorei] = useState('');
+  const caloreiChangeHandler = (event) => {
+    let newValue = event.target.value;
+    setUserCalorei(newValue);
+    console.log(`someone typed ${userCalorei}`);
+  };
+
   return (
     <form>
       <div className="add-metric__elements">
         <div className="add-metric__element">
           <label>Date</label>
-          <input type="datetime-local" />
+          <input type="datetime-local" onChange={dateChangeHandler} />
         </div>
         <div className="add-metric__element">
           <label>Name</label>
-          <input type="text" />
+          <input type="text" onChange={nameChangeHandler} />
         </div>
         <div className="add-metric__element">
           <label>Meal</label>
-          <input type="text" />
+          <input type="text" onChange={mealChangeHandler} />
         </div>
         <div className="add-metric__element">
           <label>Amount</label>
-          <input type="text" />
+          <input type="text" onChange={amountChangeHandler} />
         </div>
         <div className="add-metric__element">
           <label>Calorei</label>
-          <input type="number" min="0" step="1" />
+          <input
+            type="number"
+            min="0"
+            step="1"
+            onChange={caloreiChangeHandler}
+          />
         </div>
       </div>
       <div className="add-metric__actions">
