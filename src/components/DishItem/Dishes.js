7e82022fe0639675fc9f988ece3fc9f182da6e8e
HabibehKhavari <h.khavari1995@gmail.com>
2021-06-13 13:03:43 +0300

    render array using map and current item

--- src/components/DishItem/Dishes.js
+++ src/components/DishItem/Dishes.js
@@ -6,13 +6,13 @@ import Card from '../UI/Card';
 const Dishes = (props) => {
   return (
     <Card className="metrics">
-      {props.data.map((item, index) => {
+      {props.data.map((item) => {
         return <DishItem
-          userTimestamp={props.data[index].timestamp}
-          userName={props.data[index].name}
-          userMealtype={props.data[index].mealtype}
-          userCalories={props.data[index].calories}
-          userDescription={props.data[index].description}
+          userTimestamp={item.timestamp}
+          userName={item.name}
+          userMealtype={item.mealtype}
+          userCalories={item.calories}
+          userDescription={item.description}
         />
       })
       }
